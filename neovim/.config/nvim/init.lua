ac052642f6c978b0120da115ec09f8fd4855c225
Tom van Dijk <18gatenmaker6@gmail.com>
2021-07-06 10:18:56 +0200

    Made it stow.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,6 +3,17 @@
   -- /  |/ / __/ / / / / | / // // /|_/ /
  -- / /|  / /___/ /_/ /| |/ // // /  / /
 -- /_/ |_/_____/\____/ |___/___/_/  /_/
+
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+	fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+	execute 'packadd packer.nvim'
+end
+
 package.loaded.globals = nil
 require("globals")
 R("plugins")
