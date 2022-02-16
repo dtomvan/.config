59b6ca458d3e4b92d9393d00ac90f22793eae37e
Tom van Dijk <18gatenmaker6@gmail.com>
2022-02-16 12:17:41 +0100

    Restore original bg.sh

--- xorg/bg.sh
+++ xorg/bg.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 # Uses xwinwrap to display given animated .gif in the center of the screen
 
-#if [ $# -ne 1 ]; then
-#	echo 1>&2 Usage: $0 image.gif
-#	exit 1
-#fi
+if [ $# -ne 1 ]; then
+	echo 1>&2 Usage: $0 image.gif
+	exit 1
+fi
 
-##get screen resolution
-#SCRH=`xrandr | awk '/current/ { print $8 }'`
-#SCRW=`xrandr | awk '/current/ { print $10 }'`
-#SCRW=${SCRW%\,}
+#get screen resolution
+SCRH=`xrandr | awk '/current/ { print $8 }'`
+SCRW=`xrandr | awk '/current/ { print $10 }'`
+SCRW=${SCRW%\,}
 
-##get gif resolution
-#IMGHW=`gifsicle --info $1 | awk '/logical/ { print $3 }'`
-#IMGH=${IMGHW%x*}
-#IMGW=${IMGHW#*x}
+#get gif resolution
+IMGHW=`gifsicle --info $1 | awk '/logical/ { print $3 }'`
+IMGH=${IMGHW%x*}
+IMGW=${IMGHW#*x}
 
-##calculate position
-#POSH=$((($SCRH/2)-($IMGH/2)))
-#POSW=$((($SCRW/2)-($IMGW/2)))
+#calculate position
+POSH=$((($SCRH/2)-($IMGH/2)))
+POSW=$((($SCRW/2)-($IMGW/2)))
 
-# xwinwrap -g ${IMGHW}+${POSH}+${POSW} -ov -ni -s -nf -- gifview -w WID $1 -a
-xwinwrap -ov -ni -s -nf -g 1920x1057+0+23 -- ~/app/app/app/app -w WID
+xwinwrap -g ${IMGHW}+${POSH}+${POSW} -ov -ni -s -nf -- gifview -w WID $1 -a
