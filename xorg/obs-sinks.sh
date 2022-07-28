32b95123cd15ceb8d39ceec267be0c0b44b191fe
Tom van Dijk <18gatenmaker6@gmail.com>
2022-07-28 18:30:38 +0200

    2022-07-28

---
+++ xorg/obs-sinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEVICE="alsa_output.pci-0000_0b_00.3.analog-stereo"
+
+pactl load-module module-remap-sink sink_name="OBS_output" \
+master="$DEVICE" channels=2 \
+master_channel_map=front-left,front-right channel_map=front-left,front-right \
+remix=no sink_properties=device.description="OBS Base Output"
+# pacmd update-sink-proplist base_output device.description="Base_Sink"
+pactl load-module module-remap-sink sink_name="Music_output" \
+master="$DEVICE" channels=2 \
+master_channel_map=front-left,front-right channel_map=front-left,front-right \
+remix=no sink_properties=device.description="OBS Music Output"
+# pacmd update-sink-proplist music_output device.description="Music_Sink"
