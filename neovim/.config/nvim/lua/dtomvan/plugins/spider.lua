6f0277357ea58a6e096cc9bce70f3394d201a8a5
Tom van Dijk <18gatenmaker6@gmail.com>
2023-04-19 21:28:03 +0200

    2023-04-19

--- neovim/.config/nvim/lua/dtomvan/plugins/spider.lua
+++ neovim/.config/nvim/lua/dtomvan/plugins/spider.lua
@@ -1,20 +1,33 @@
-M = {
+local keymaps = require 'dtomvan.keymaps'
+local treesitter_fts
+
+return {
     'chrisgrieser/nvim-spider',
     opts = {
         skipInsignificantPunctuation = true,
     },
-    keys = {},
+    keys = { 'w', 'e', 'b', 'ge' },
+    init = function()
+        for _, i in ipairs { 'w', 'e', 'b', 'ge' } do
+            keymaps.confusing({ 'n', 'x' }, i,
+                function()
+                    if not treesitter_fts then
+                        treesitter_fts =
+                            vim.tbl_keys(
+                                require 'nvim-treesitter.parsers'.get_parser_configs()
+                            )
+                    end
+                    if treesitter_fts[vim.bo.ft] then
+                        require('spider').motion(i)
+                        return ''
+                    end
+                    return i
+                end,
+                {
+                    desc = 'Spider-' .. i,
+                    expr = true,
+                }
+            )
+        end
+    end
 }
-
-for _, i in ipairs { 'w', 'e', 'b', 'ge' } do
-    table.insert(M.keys, {
-        i,
-        function()
-            require('spider').motion(i)
-        end,
-        mode = { 'n', 'x' },
-        desc = 'Spider-' .. i,
-    })
-end
-
-return M
